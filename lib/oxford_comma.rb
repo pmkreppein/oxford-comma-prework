9f31dc2a8c200e2f057b2d837ba21bea0d1027e3
Peter Kreppein <pmkreppein@gmail.com>
2018-09-22 05:53:12 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
-def oxford_comma(array)
 
-end
+def oxford_comma(input)
+    
+    
+    if input.length == 2
+      return "#{input[0]} and #{input[1]}"
+     elsif input.length > 2
+    input[-1].insert(0, "and ")
+    end
+    input.join(", ")    
+  end
